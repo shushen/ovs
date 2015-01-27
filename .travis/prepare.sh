fac9903d69f0cf337b54252ce9bcfaadf8c6ddd8
Shu Shen <shu.shen@radisys.com>
2015-01-27 13:17:27 -0800

    travis: add dependency for make dist-docs
    Signed-off-by: Shu Shen <shu.shen@radisys.com>

--- .travis/prepare.sh
+++ .travis/prepare.sh
@@ -3,6 +3,8 @@
 sudo apt-get update -qq
 sudo apt-get install -qq libssl-dev llvm-dev
 sudo apt-get install -qq graphviz groff ghostscript
+sudo apt-get install -qq ghostscript markdown
+sudo apt-get install -qq python
 
 git clone git://git.kernel.org/pub/scm/devel/sparse/chrisl/sparse.git
 cd sparse && make && sudo make install PREFIX=/usr && cd ..
